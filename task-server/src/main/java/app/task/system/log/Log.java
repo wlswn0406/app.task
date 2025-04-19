7b7aa8911841a805132bc3f70d5590d6b8b01a67
--global <yoojj.dev@gmail.com>
2025-04-19 10:05:17 +0900

    refactor: 필드 수정 및 주석 추가

--- task-server/src/main/java/app/task/system/log/Log.java
+++ task-server/src/main/java/app/task/system/log/Log.java
@@ -4,12 +4,17 @@ import java.time.LocalDateTime;
 
 /**
  * 시스템 로그 도메인
- * 
+ *
+ * @param referenceId 로그가 참조하는 대상 테이블의 기본키
+ * @param tableName 로그가 발생한 테이블 이름
+ * @param logMessage 로그 상세 메시지
+ * @param actor 로그 발생 주체 (SYSTEM or 사용자 아이디)
+ * @param createdAt 로그 생성 시각
  */
 public record Log(
     Integer id,
     Integer referenceId,
-    LogType logType,
+    TableName tableName,
     String logMessage,
     String actor,
     LocalDateTime createdAt
