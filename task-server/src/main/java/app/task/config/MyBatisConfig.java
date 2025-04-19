f2a621c06c786a01e4f2c5e1329436c25d80a947
--global <yoojj.dev@gmail.com>
2025-04-19 17:04:28 +0900

    chore: 클래스 설명 추가

--- task-server/src/main/java/app/task/config/MyBatisConfig.java
+++ task-server/src/main/java/app/task/config/MyBatisConfig.java
@@ -3,6 +3,10 @@ package app.task.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * MyBatis 매퍼 인터페이스를 스캔하기 위한 설정 클래스
+ *
+ */
 @Configuration
 @MapperScan("app.task")
 public class MyBatisConfig {
