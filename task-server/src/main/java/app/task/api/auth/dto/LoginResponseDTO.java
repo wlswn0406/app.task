da195d3c3deae76b5712a1744481bb8fd8e13b44
--global <yoojj.dev@gmail.com>
2025-04-19 11:39:21 +0900

    feat: Auth 관련 DTO 생성

---
+++ task-server/src/main/java/app/task/api/auth/dto/LoginResponseDTO.java
@@ -0,0 +1,19 @@
+package app.task.api.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponseDTO {
+
+    private String token;
+    private String id;
+
+}
---
+++ task-server/src/main/java/app/task/api/auth/dto/SignupDTO.java
@@ -0,0 +1,23 @@
+package app.task.api.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SignupDTO {
+
+    @NotBlank(message = "아이디는 필수입니다")
+    private String id;
+
+    @NotBlank(message = "비밀번호는 필수입니다")
+    private String password;
+
+}
