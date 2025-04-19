f42e9a0bdaf944b1e089982f9138b441f8c170e1
--global <yoojj.dev@gmail.com>
2025-04-19 17:01:08 +0900

    refactor: json 관리를 위한 빈 추가

--- task-server/src/main/java/app/task/config/AppConfig.java
+++ task-server/src/main/java/app/task/config/AppConfig.java
@@ -1,15 +1,25 @@
 package app.task.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 애플리케이션 전반에서 사용할 빈을 등록하는 설정 클래스
+ *
+ */
 @Configuration
 public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 
 }
